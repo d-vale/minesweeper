cff7debd9eb3ffc199d178334170f57d4339fc7b
Dvale <vale.daniel@outlook.com>
2024-12-12 20:17:58 +0100

    Ajout fichier config

--- config/config.exs
+++ config/config.exs
@@ -1,41 +1,45 @@
-# This file is responsible for configuring your application and its dependencies
-# with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and is restricted to
-# this project.
-
 import Config
 
-config :minesweeper,
-  ecto_repos: [Minesweeper.Repo]
-
-# Configure the database connection URL.
-config :minesweeper, Minesweeper.Repo, url: "ecto://minesweeper@localhost/minesweeper"
+# Configure the database connection.
+#
+# The various connection parameters (host, port, username, password, database
+# name) are provided by specifying an URL (see
+# https://hexdocs.pm/ecto/Ecto.Repo.html#module-urls for more information).
+#
+# The format is:  "ecto://<username>[:<password>]@<host>[:<port>]/<database-name>"
+# For example:    "ecto://minesweeper:mysecretpassword@localhost:5432/minesweeper"
+config :minesweeper, Minesweeper.Repo,
+  url:
+    if(config_env() != :test,
+      # This is the production database URL. You can override it at runtime with
+      # the $MINESWEEPER_DATABASE_URL environment variable.
+      do: "ecto://minesweeper:Archidep.10@localhost/minesweeper",
+      # This is a separate test database URL to avoid overwriting
+      # development/production data during tests. You only need to configure
+      # this if you want to run the automated tests.
+      else: "ecto://minesweeper:Archidep.10@localhost/minesweeper-test"
+    )
 
 # Configure the web endpoint.
 config :minesweeper, MinesweeperWeb.Endpoint,
-  http: [port: 3000],
-  pubsub_server: Minesweeper.PubSub,
-  render_errors: [view: MinesweeperWeb.Errors.ErrorsView, accepts: ~w(html json), layout: false],
-  server: true,
+  http: [
+    # Set the HTTP port the server listens on.
+    port: 3002
+  ],
+  # The secret key base is used as a base to generate secrets for encrypting and
+  # signing data. For example, cookies and tokens are signed by default. This
+  # should be a long random string.
+  #
+  # You can override it at runtime with the $MINESWEEPER_SECRET_KEY_BASE
+  # environment variable.
+  #
+  # Run "mix phx.gen.secret" at the root of this repository to generate a
+  # suitable value for the secret key base.
+  secret_key_base: "e1VywzpRp7U9atWDWwSEt7LKW9QoD/17Zaw4brCmSUwHkAmMNUT/M0eBkwG0Uxnc",
+  # The base URL at which the application is publicly available. You would
+  # typically use the same host as in the configuration of your reverse proxy.
   url: [
-    host: "localhost",
-    port: 3000
+    host: "minesweeper.dpe.archidep2.ch",
+    port: 443
   ]
 
-# Configures Elixir's Logger.
-config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
-  metadata: [:request_id]
-
-# Use Jason for JSON parsing in Phoenix.
-config :phoenix, :json_library, Jason
-
-config_dir = Path.dirname(__ENV__.file)
-
-# Import environment specific config. This must remain at the bottom of this
-# file so it overrides the configuration defined above.
-import_config Path.join(config_dir, "#{Mix.env()}.exs")
-
-local_config_file = Path.join(config_dir, "local.exs")
-if File.exists?(local_config_file), do: import_config(local_config_file)
